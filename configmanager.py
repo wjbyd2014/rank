f3f5443f17e7fd757d365b9cf249252c71d05ef9
wjbyd2014 <176930292@qq.com>
2022-07-22 22:28:22 +0800

    Create configmanager.py

---
+++ configmanager.py
@@ -0,0 +1,81 @@
+class ConfigManager:
+    def __init__(self, log_path):
+        self.list_factors = list()
+        self.config_values = dict()
+        self.log_fd = open(log_path, 'w')
+
+    def add_factor1(self, name, min_value, max_value, interval):
+        self.list_factors.append(self.分箱(min_value, max_value, interval))
+        self.config_values[name] = None
+
+    def add_factor2(self, name, factors):
+        self.list_factors.append(factors)
+        self.config_values[name] = None
+
+    @staticmethod
+    def 分箱(min_value, max_value, interval):
+        ret = list()
+        value = min_value
+        while value <= max_value:
+            ret.append(value)
+            value += interval
+        return ret
+
+    def get_config_value(self, config_name):
+        return self.config_values[config_name]
+
+    def update_configs(self, list_config_values):
+        assert (len(list_config_values) == len(self.config_values))
+        i = 0
+        for config_name in self.config_values:
+            self.config_values[config_name] = list_config_values[i]
+            i += 1
+
+    def gen_factors(self):
+        return self.__gen_factors(self.list_factors)
+
+    def __gen_factors(self, list_params):
+        if len(list_params) == 1:
+            for param in list_params[0]:
+                yield [param]
+        else:
+            list_factor = self.__gen_factors(list_params[1:])
+            for factor in list_factor:
+                for param in list_params[0]:
+                    yield [param] + factor
+
+    def len_factors(self):
+        ret = 1
+        for factors in self.list_factors:
+            ret *= len(factors)
+        return ret
+
+    def log(self, max_total_earn_money):
+        log_str = ''
+        if self.log_fd:
+            for config_value in self.config_values.values():
+                log_str += str(config_value) + ' '
+            log_str += '%d\n' % max_total_earn_money
+            self.log_fd.write(log_str)
+            self.log_fd.flush()
+
+    def __del__(self):
+        if self.log_fd:
+            self.log_fd.close()
+
+
+if __name__ == '__main__':
+    cm = ConfigManager('cm.txt')
+    cm.add_factor1('v1', 1, 2, 1)
+    cm.add_factor1('v2', 3, 4, 1)
+    cm.add_factor1('v3', 5, 6, 1)
+    cm.add_factor2('v4', ['a','b'])
+    print(cm.len_factors())
+
+    list_factors = cm.gen_factors()
+    i = 0
+    for factors in list_factors:
+        i += 1
+        cm.update_configs(factors)
+        cm.log(1.2345)
+        print(cm.get_config_value('v1'), ' ', cm.get_config_value('v2'), ' ', cm.get_config_value('v3'), ' ', cm.get_config_value('v4'))
