b07b3b490491665e0cfcb01ddabfe9f559ea7c29
wjbyd2014 <176930292@qq.com>
2022-07-27 10:14:27 +0800

    Update 新面积策略.py

--- 新面积策略.py
+++ 新面积策略.py
@@ -14,8 +14,9 @@ def count_buy_amount(data_list):
     for data in data_list:
         data['可买金额'] = data['计划买入金额'] = data['盈亏金额'] = data['盈亏比'] = 0
 
-        if data['买入量'] > 0 and data['买入价'] > 0:
-            data['买入价'] *= 1.002
+        assert (data['买入价'] > 0)
+
+        if data['买入量'] > 0:
             data['可买金额'] = round(data['买入价'] * data['买入量'])
             data['计划买入金额'] = data['可买金额'] * (cm.get_config_value('买入比') / 100)
 
@@ -26,13 +27,13 @@ def count_buy_amount(data_list):
             data['盈亏金额'] = round((data['卖出价'] - data['买入价']) * data['买入量'])
 
 
-def count_stock_area_earn_money(data_list, writer):
+def count_stock_area_earn_money(data_list, writer, 回测模式):
     ret = 0
     left_money = 每日资金量 * 10000
     min_use_money_per_stock = cm.get_config_value('每只股票最小购买金额') * 10000
 
     for data in data_list:
-        if data['买入价'] != 0 and left_money > 0:
+        if left_money > 0:
             if data['计划买入金额'] > left_money:
                 data['实际买入金额'] = left_money
             else:
@@ -53,7 +54,10 @@ def count_stock_area_earn_money(data_list, writer):
         else:
             data['实际买入金额'] = data['实际盈亏金额'] = 0
 
-        if writer:
+            if 回测模式:
+                break
+
+        if 回测模式:
             data['买入价'] = round(data['买入价'], 3)
             data['实际买入金额'] = round(data['实际买入金额'])
             data['实际盈亏金额'] = round(data['实际盈亏金额'])
@@ -63,16 +67,16 @@ def count_stock_area_earn_money(data_list, writer):
 
 def select_stocks(data_list):
     for data in data_list:
-        data['打分'] = data['总面积']
+        data['打分'] = data['平均面积']
 
         if data['1日涨停板数'] > 0:
             data['打分'] += cm.get_config_value('涨停板数1打分')
-        elif data['3日涨停板数'] > 0:
-            data['打分'] += cm.get_config_value('涨停板数3打分')
-        elif data['5日涨停板数'] > 0:
-            data['打分'] += cm.get_config_value('涨停板数5打分')
-        elif data['7日涨停板数'] > 0:
-            data['打分'] += cm.get_config_value('涨停板数7打分')
+        if data['3日涨停板数'] > 0:
+            data['打分'] += cm.get_config_value('涨停板数3打分') * (data['3日涨停板数'] - data['1日涨停板数'])
+        if data['5日涨停板数'] > 0:
+            data['打分'] += cm.get_config_value('涨停板数5打分') * (data['5日涨停板数'] - data['3日涨停板数'])
+        if data['7日涨停板数'] > 0:
+            data['打分'] += cm.get_config_value('涨停板数7打分') * (data['7日涨停板数'] - data['5日涨停板数'])
 
     data_list.sort(key=lambda x: x['打分'], reverse=True)
 
@@ -80,15 +84,15 @@ def select_stocks(data_list):
 每日资金量 = 6000
 
 cm = ConfigManager(work_dir + '回测.txt')
-cm.add_factor1('涨停板数1打分', 86.5, 86.5, 0.5)
-cm.add_factor1('涨停板数3打分', 220, 220, 0.5)
-cm.add_factor1('涨停板数5打分', 115.5, 115.5, 0.5)
-cm.add_factor1('涨停板数7打分', 0, 0, 1)
+cm.add_factor1('涨停板数1打分', 8, 8, 0.5)
+cm.add_factor1('涨停板数3打分', 4, 4, 0.5)
+cm.add_factor1('涨停板数5打分', 2, 2, 0.5)
+cm.add_factor1('涨停板数7打分', 1, 1, 0.5)
 cm.add_factor1('最小上市天数', 35, 35, 1)
 cm.add_factor1('最小量比', 5.5, 5.5, 0.5)
-cm.add_factor1('每只股票最大购买金额', 3600, 3600, 100)
-cm.add_factor1('每只股票最小购买金额', 170, 170, 10)
-cm.add_factor1('买入比', 50, 50, 1)
+cm.add_factor1('每只股票最大购买金额', 1800, 1800, 100)
+cm.add_factor1('每只股票最小购买金额', 100, 100, 10)
+cm.add_factor1('买入比', 100, 100, 1)
 
 
 def 运行新面积策略(回测模式):
@@ -132,7 +136,7 @@ def 运行新面积策略(回测模式):
         writer = csv.DictWriter(fd, fieldnames=field_names)
         writer.writeheader()
 
-    ret_date = ts.get_dates(20220726)
+    ret_date = ts.get_dates(20220718)
     ret_date.reverse()
 
     ts_dates = [date['date'] for date in ret_date]
@@ -184,7 +188,7 @@ def 运行新面积策略(回测模式):
                 date_to_stock_data[date] = data_list
 
             select_stocks(date_to_stock_data[date])
-            got_money, left_money = count_stock_area_earn_money(date_to_stock_data[date], writer)
+            got_money, left_money = count_stock_area_earn_money(date_to_stock_data[date], writer, 回测模式)
             if left_money > 0:
                 print("%s left %d\n" % (date, left_money))
             earn_money[date] = got_money
