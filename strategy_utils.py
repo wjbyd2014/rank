239607b2aa35d739f6f1bf4a5f7ee1623461dce4
wjbyd2014 <176930292@qq.com>
2022-07-29 11:15:29 +0800

    多图对比

--- strategy_utils.py
+++ strategy_utils.py
@@ -42,3 +42,58 @@ def draw_earn_money(day_earn_money, work_dir, title, show_picture):
         f = plt.gcf()  # 获取当前图像
         path = work_dir + '{}.png'.format(title)
         f.savefig(path)
+
+
+def draw_list_earn_money(list_day_earn_money, work_dir, title, show_picture):
+    ymax = ymin = None
+    xmax = xmin = None
+
+    list_d = list()
+    for day_earn_money in list_day_earn_money:
+        earn_money = 0
+        d = dict()
+        for day in day_earn_money:
+            earn_money += day_earn_money[day]
+            d[pd.to_datetime(str(day))] = [earn_money / 10000]
+
+            if not ymax:
+                ymax = ymin = earn_money / 10000
+            else:
+                if ymax < earn_money / 10000:
+                    ymax = earn_money / 10000
+                if ymin > earn_money / 10000:
+                    ymin = earn_money / 10000
+        if not xmax:
+            xmax = list(d.keys())[-1]
+            xmin = list(d.keys())[0]
+        list_d.append(d)
+
+    matplotlib.rcParams['font.sans-serif'] = ['SimHei']
+    matplotlib.rcParams['font.family'] = 'sans-serif'
+    matplotlib.rcParams['axes.unicode_minus'] = False
+
+    fig, ax = plt.subplots(figsize=(14, 7))
+    d_keys = list(list_d[0].keys())
+    d_values = [d.values() for d in list_d]
+    for values in d_values:
+        ax.plot(d_keys, values)
+
+    alldays = matplotlib.dates.DayLocator(interval=5)  # 主刻度为每月
+    ax.xaxis.set_major_locator(alldays)  # 设置主刻度
+    ax.xaxis.set_major_formatter(matplotlib.dates.DateFormatter('%Y%m%d'))  # 主刻度格式为年月日
+    ax.xaxis.set_minor_formatter(plt.NullFormatter())  # 取消副刻度
+    ax.set_xlim(xmin, xmax)  # x轴范围
+    ax.set_ylim(ymin, ymax)  # y轴范围
+    ax.set_yticks([*np.linspace(ymin, ymax, 20)])
+    plt.subplots_adjust(top=0.96, bottom=0.09, right=0.97, left=0.03, hspace=0.02, wspace=0.02)
+    plt.title(title)
+    plt.grid()
+    plt.xticks(fontsize=8, rotation=90)
+    plt.yticks(fontsize=8)
+
+    if show_picture:
+        plt.show()
+    else:
+        f = plt.gcf()  # 获取当前图像
+        path = work_dir + '{}.png'.format(title)
+        f.savefig(path)
