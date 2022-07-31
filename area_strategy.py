8e13ff2e871d4fba17f76100a8a126b95b56186e
wjbyd2014 <176930292@qq.com>
2022-07-31 12:27:09 +0800

    调整csv字段顺序，观察期结束打板的不再纳入股票池

--- area_strategy.py
+++ area_strategy.py
@@ -4,10 +4,12 @@ from strategy import *
 class AreaStrategy(Strategy):
     def __init__(self, name, work_dir, csv_file_name, csv_field_names,
                  stock_pool_file_name, stock_pool_fields, stock_pool_js_file_name, stock_pool_js_params,
-                 stock_info_file_name, stock_info_fields, stock_info_js_file_name, stock_info_js_params):
+                 stock_info_file_name, stock_info_fields, stock_info_js_file_name, stock_info_js_params,
+                 priority_fields, skipped_csv_fields):
         super().__init__(name, work_dir, csv_file_name, csv_field_names,
                          stock_pool_file_name, stock_pool_fields, stock_pool_js_file_name, stock_pool_js_params,
-                         stock_info_file_name, stock_info_fields, stock_info_js_file_name, stock_info_js_params)
+                         stock_info_file_name, stock_info_fields, stock_info_js_file_name, stock_info_js_params,
+                         priority_fields, skipped_csv_fields)
 
     def __del__(self):
         super().__del__()
@@ -27,10 +29,6 @@ class AreaStrategy(Strategy):
 
             data['打分'] += self.cm.get_config_value('最低点系数') * data['最低点']
             data['打分'] += self.cm.get_config_value('最高点系数') * data['最高点']
-
-            if data['观察期结束是否涨停'] == 1:
-                data['打分'] = 0
-                continue
 
             if data['买入量'] == 0:
                 data['打分'] = 0
@@ -69,10 +67,10 @@ if __name__ == '__main__':
                                  ['打分', '当日排名'],
                                  '新面积策略股票池.csv',
                                  {'日期': str, '代码': str, '名称': str, '量比': float,
-                                  '买入量': float, '买入价': float, '观察期结束是否涨停': int,
+                                  '买入量': float, '买入价': float,
                                   '交叉点': str, '总面积': float, '平均面积': float,
-                                  '1日涨停板数': int, '3日涨停板数': int, '5日涨停板数': int, '7日涨停板数': int,
                                   '开板次数': int, '开板最大回撤': float, '最高点': float, '最低点': float,
+                                  '1日涨停板数': int, '3日涨停板数': int, '5日涨停板数': int, '7日涨停板数': int, '10日涨停板数': int
                                   },
                                  'mianji_stock_poll.js',
                                  {'time1': '09:33:00', 'time2': '09:53:00',
@@ -83,7 +81,9 @@ if __name__ == '__main__':
                                   '开盘价涨幅': float, '昨日是否一字板': int
                                   },
                                  'mianji_stock_info.js',
-                                 {})
+                                 {},
+                                 ['日期', '代码', '名称', '可买金额', '盈亏金额', '计划买入金额', '实际买入金额', '实际盈亏金额', '买入价', '卖出价', '卖出日期'],
+                                 ['买入量'])
     area_strategy.init()
     area_strategy.add_factor2('涨停板数1打分', [3.71])
     area_strategy.add_factor2('涨停板数3打分', [1.58])
