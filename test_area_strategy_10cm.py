cc26f78efc2b87979169e84a1e5bd9a2bececfd6
wjbyd2014 <176930292@qq.com>
2022-08-04 20:27:28 +0800

    添加新因子，客单价129

--- test_area_strategy_10cm.py
+++ test_area_strategy_10cm.py
@@ -5,11 +5,11 @@ class AreaStrategy(Strategy):
     def __init__(self, name, work_dir, csv_file_name, csv_field_names,
                  stock_pool_file_name, stock_pool_fields, stock_pool_js_file_name, stock_pool_js_params,
                  stock_info_file_name, stock_info_fields, stock_info_js_file_name, stock_info_js_params,
-                 priority_fields, skipped_csv_fields):
+                 priority_fields, skipped_csv_fields, begin_date, date_num):
         super().__init__(name, work_dir, csv_file_name, csv_field_names,
                          stock_pool_file_name, stock_pool_fields, stock_pool_js_file_name, stock_pool_js_params,
                          stock_info_file_name, stock_info_fields, stock_info_js_file_name, stock_info_js_params,
-                         priority_fields, skipped_csv_fields)
+                         priority_fields, skipped_csv_fields, begin_date, date_num)
 
     def __del__(self):
         super().__del__()
@@ -28,6 +28,12 @@ class AreaStrategy(Strategy):
                 data['打分'] += self.cm.get_config_value('涨停板数7打分') * (data['7日涨停板数'] - data['5日涨停板数'])
             if data['10日涨停板数'] > 0:
                 data['打分'] += self.cm.get_config_value('涨停板数10打分') * (data['10日涨停板数'] - data['7日涨停板数'])
+
+            data['打分'] += data['1日低位涨停板数'] * self.cm.get_config_value('1日低位涨停板数打分')
+            data['打分'] += data['3日低位涨停板数'] * self.cm.get_config_value('3日低位涨停板数打分')
+            data['打分'] += data['5日低位涨停板数'] * self.cm.get_config_value('5日低位涨停板数打分')
+            data['打分'] += data['7日低位涨停板数'] * self.cm.get_config_value('7日低位涨停板数打分')
+            data['打分'] += data['10日低位涨停板数'] * self.cm.get_config_value('10日低位涨停板数打分')
 
             data['打分'] += self.cm.get_config_value('最低点系数') * data['最低点']
             data['打分'] += self.cm.get_config_value('最高点系数') * data['最高点']
@@ -97,13 +103,15 @@ if __name__ == '__main__':
                                  '新面积策略股票信息.csv',
                                  {'名称': str, '代码': str, '上市天数': float, 'ma3向上': int, 'ma5向上': int,
                                   '上涨起点日': str, '涨板打断次数': int,
-                                  '开盘价涨幅': float, '昨日是否一字板': int
+                                  '开盘价涨幅': float, '昨日是否一字板': int,
+                                  '1日低位涨停板数': int, '3日低位涨停板数': int, '5日低位涨停板数': int,
+                                  '7日低位涨停板数': int, '10日低位涨停板数': int
                                   },
                                  'mianji_stock_info_day.js',
                                  {},
                                  ['日期', '代码', '名称', '可买金额', '盈亏金额', '盈亏比', '计划买入金额', '实际买入金额', '实际盈亏金额',
                                   '买入价', '卖出价', '卖出日期', '打分', '当日排名'],
-                                 ['买入量'])
+                                 ['买入量'], 20220727, 300)
 
     area_strategy.add_factor2('涨停板数1打分', [1])
     area_strategy.add_factor2('涨停板数3打分', [1])
@@ -123,6 +131,11 @@ if __name__ == '__main__':
     area_strategy.add_factor2('ma5向上系数', [0.88])
     area_strategy.add_factor2('昨日一字板系数', [0.7])
     area_strategy.add_factor2('最小可买金额', [250])
+    area_strategy.add_factor2('1日低位涨停板数打分', [0])
+    area_strategy.add_factor2('3日低位涨停板数打分', [0])
+    area_strategy.add_factor2('5日低位涨停板数打分', [0])
+    area_strategy.add_factor2('7日低位涨停板数打分', [3])
+    area_strategy.add_factor2('10日低位涨停板数打分', [0])
 
     area_strategy.set_max_use_money_per_day(3000)
     area_strategy.set_data_filter(lambda data: data['代码'][2:4] in ['60', '00'])
