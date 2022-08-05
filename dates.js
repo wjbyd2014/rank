15177e19083c5d9cce453544f0856d56ec8869f3
wjbyd2014 <176930292@qq.com>
2022-08-05 08:50:04 +0800

    自定义回测天数

--- dates.js
+++ dates.js
@@ -1,11 +1,12 @@
 Function dates();
 Begin
     date_ := {day};
+    num_ := {num};
     arr := array();
     SetSysParam(PN_Stock(),'SH000001');
     setsysparam(pn_cycle(),cy_day());
     EndT:=inttodate(date_);
-    for i := 0 to 300 do
+    for i := 0 to num_ do
     begin
         dd := StockEndTPrevNDay(EndT, i);
         arr &= array(('date':DateToInt(dd), 'datestr':DateToStr(dd)));
