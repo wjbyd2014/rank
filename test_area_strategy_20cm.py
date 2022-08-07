41dea8171e048311b69636997fb7c56ca9918932
wjbyd2014 <176930292@qq.com>
2022-08-07 10:33:34 +0800

    20220807 添加淘汰原因，客单价176

--- test_area_strategy_20cm.py
+++ test_area_strategy_20cm.py
@@ -18,6 +18,7 @@ class AreaStrategy(Strategy):
         for data in data_list:
             if data['买入量'] == 0:
                 data['打分'] = 0
+                data['淘汰原因'] = '买入量'
                 continue
 
             if not self.data_filter(data):
@@ -26,34 +27,42 @@ class AreaStrategy(Strategy):
 
             if data['量比'] < self.cm.get_config_value('最小量比'):
                 data['打分'] = 0
+                data['淘汰原因'] = '最小量比'
                 continue
 
             if data['开板次数'] > self.cm.get_config_value('最大开板次数'):
                 data['打分'] = 0
+                data['淘汰原因'] = '最大开板次数'
                 continue
 
             if data['开板最大回撤'] > self.cm.get_config_value('最大开板最大回撤'):
                 data['打分'] = 0
+                data['淘汰原因'] = '最大开板最大回撤'
                 continue
 
             if data['上市天数'] < self.cm.get_config_value('最小上市天数'):
                 data['打分'] = 0
+                data['淘汰原因'] = '最小上市天数'
                 continue
 
             if data['涨板打断次数'] > self.cm.get_config_value('最大断板次数'):
                 data['打分'] = 0
+                data['淘汰原因'] = '最大断板次数'
                 continue
 
             if data['可买金额'] < self.cm.get_config_value('最小可买金额') * 10000:
                 data['打分'] = 0
+                data['淘汰原因'] = '最小可买金额'
                 continue
 
             if data['开盘最大回撤'] < self.cm.get_config_value('开盘最大回撤'):
                 data['打分'] = 0
+                data['淘汰原因'] = '开盘最大回撤'
                 continue
 
             if data['开盘价涨幅'] < self.cm.get_config_value('开盘价最小涨幅'):
                 data['打分'] = 0
+                data['淘汰原因'] = '开盘价最小涨幅'
                 continue
 
             data['打分'] = data['平均面积']
@@ -92,7 +101,7 @@ if __name__ == '__main__':
     area_strategy = AreaStrategy('area_strategy_20cm',
                                  'D:\\ts\\',
                                  'area_strategy_20cm.csv',
-                                 ['打分', '当日排名'],
+                                 ['打分', '当日排名', '淘汰原因'],
                                  '新面积策略股票池.csv',
                                  {'日期': str, '代码': str, '名称': str, '量比': float,
                                   '买入量': float, '买入价': float,
