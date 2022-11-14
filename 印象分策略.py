a9ff468bcfcbc7e7bf824855d0f3d8854ae4bf9f
wjbyd2014 <176930292@qq.com>
2022-11-14 15:21:17 +0800

    Update 印象分策略.py

--- 印象分策略.py
+++ 印象分策略.py
@@ -34,11 +34,13 @@ class StrategyYinXiang(Strategy):
         factor4 = utils.guiyihua(data_list, '阳线实体')
 
         for i in range(len(data_list)):
-            data_list[i]['印象打分'] = factor1[i]
-            data_list[i]['相对大盘涨幅打分'] = factor2[i]
-            data_list[i]['换手率比打分'] = factor3[i]
-            data_list[i]['阳线实体打分'] = factor4[i]
-            data_list[i]['score'] = factor1[i] + factor2[i] + factor3[i] + factor4[i]
+            data_list[i]['印象打分'] = round(factor1[i] * self.cm.get_config_value('印象分系数'), 3)
+            data_list[i]['相对大盘涨幅打分'] = round(factor2[i] * self.cm.get_config_value('相对大盘涨幅系数'), 3)
+            data_list[i]['换手率比打分'] = round(factor3[i] * self.cm.get_config_value('换手率比系数'), 3)
+            data_list[i]['阳线实体打分'] = round(factor4[i] * self.cm.get_config_value('阳线实体系数'), 3)
+            data_list[i]['score'] =\
+                round(data_list[i]['印象打分'] + data_list[i]['相对大盘涨幅打分'] +
+                      data_list[i]['换手率比打分'] + data_list[i]['阳线实体打分'], 3)
 
     def select_stocks(self, data_list):
         super().select_stocks(data_list)
@@ -53,9 +55,11 @@ class StrategyYinXiang(Strategy):
             elif data['过去2到10天最大成交金额'] < self.cm.get_config_value('最小过去2到10天最大成交金额'):
                 data['淘汰原因'] = '过去2到10天最大成交金额'
                 break
-            elif data['10日涨停数'] <= self.cm.get_config_value('最小10日涨停数'):
+            elif data['10日涨停数'] < self.cm.get_config_value('最小10日涨停数'):
                 data['淘汰原因'] = '10日涨停数'
                 break
+            elif data['上市天数'] < self.cm.get_config_value('最小上市天数'):
+                data['淘汰原因'] = '上市天数'
 
 
 if __name__ == '__main__':
@@ -69,7 +73,8 @@ if __name__ == '__main__':
                                           '换手率比', '买入价', '是否买入'],
                                          '印象分策略股票池.csv',
                                          {'日期': str, '代码': str, '名称': str,
-                                          '昨日涨幅': float, '相对大盘涨幅': float, '昨日成交金额': float, '过去2到10天最大成交金额': float,
+                                          '昨日成交金额': float, '过去2到10天最大成交金额': float,
+                                          '昨日涨幅': float, '相对大盘涨幅': float,
                                           '7日涨停数': int, '10日涨停数': int, '15日涨停数': int,
                                           '创10日新高': int, '创15日新高': int,
                                           'ma5上涨起始日': str, 'ma10上涨起始日': str,
@@ -80,15 +85,20 @@ if __name__ == '__main__':
                                           },
                                          'yinxiang.js',
                                          {}, '', {}, '', {},
-                                         ['日期', '代码', '名称'],
+                                         ['日期', '代码', '名称',
+                                          '可买金额', '盈亏比', '计划买入金额', '计划买入盈亏金额', '实际买入金额', '实际盈亏金额',
+                                          '当日排名', '淘汰原因', '是否买入', 'score',
+                                          '印象打分', '阳线实体打分', '相对大盘涨幅打分', '换手率比打分',
+                                          '印象', '阳线实体', '相对大盘涨幅', '换手率比',
+                                          '昨日成交金额', '过去2到10天最大成交金额', 'ma5上涨起始日', 'ma10上涨起始日'],
                                          ['买入量', '开盘价'], 20221108, 400)
 
     yinxiang_strategy.set_sort_data_list(sort_data_list)
 
     yinxiang_strategy.init()
     yinxiang_strategy.load_data()
-    """factors = ConfigManager.linspace2(zhuiban_strategy.get_data(), '开盘价涨幅', 10)
-    zhuiban_strategy.add_factor2('开盘价涨幅范围', factors)"""
+    """factors = ConfigManager.linspace2(yinxiang_strategy.get_data(), '开盘涨幅', 10)
+    yinxiang_strategy.add_factor2('开盘价涨幅范围', factors)"""
     yinxiang_strategy.add_factor2('每日资金总量', [6000])
     yinxiang_strategy.add_factor2('单只股票购买上限', [1600])
     yinxiang_strategy.add_factor2('买入比', [100])
@@ -97,6 +107,11 @@ if __name__ == '__main__':
     yinxiang_strategy.add_factor2('最小昨日成交金额', [400000000])
     yinxiang_strategy.add_factor2('最小过去2到10天最大成交金额', [400000000])
     yinxiang_strategy.add_factor2('最小10日涨停数', [1])
+    yinxiang_strategy.add_factor2('最小上市天数', [0])
+    yinxiang_strategy.add_factor2('印象分系数', [1])
+    yinxiang_strategy.add_factor2('相对大盘涨幅系数', [7.5])
+    yinxiang_strategy.add_factor2('换手率比系数', [1.5])
+    yinxiang_strategy.add_factor2('阳线实体系数', [2.5])
     yinxiang_strategy.gen_factors()
 
     len_factors = yinxiang_strategy.len_factors()
